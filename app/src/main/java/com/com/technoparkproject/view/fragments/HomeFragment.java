06ae297273e7bf3195560a93a071edabf7a09c8f
siiena25 <yuliacoyulia@yandex.ru>
2020-11-01 23:57:29 +0300

    Error correction

--- app/src/main/java/com/com/technoparkproject/view/fragments/HomeFragment.java
+++ app/src/main/java/com/com/technoparkproject/view/fragments/HomeFragment.java
@@ -4,19 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.com.technoparkproject.R;
-import com.com.technoparkproject.view.activities.MainActivity;
 
 public class HomeFragment extends Fragment {
-
-    private View viewContent;
 
     @Nullable
     @Override
@@ -24,3 +19,4 @@ public class HomeFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
+
