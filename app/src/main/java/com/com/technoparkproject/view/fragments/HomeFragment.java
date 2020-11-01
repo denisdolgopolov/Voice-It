8945f59796ab7d7a7150b44d73f3881b5a833013
siiena25 <yuliacoyulia@yandex.ru>
2020-11-01 23:34:24 +0300

    Add fragments from bottom navigation

---
+++ app/src/main/java/com/com/technoparkproject/view/fragments/HomeFragment.java
@@ -0,0 +1,26 @@
+package com.com.technoparkproject.view.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import com.com.technoparkproject.R;
+import com.com.technoparkproject.view.activities.MainActivity;
+
+public class HomeFragment extends Fragment {
+
+    private View viewContent;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
