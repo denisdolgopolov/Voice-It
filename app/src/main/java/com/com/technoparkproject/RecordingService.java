279bd1feaa37bf6712403f0f9bcc11396d1b9943
Vadim <vad99lord@gmail.com>
2020-11-06 21:19:07 +0300

    Add basic functionality - PCM recording, foreground RecordingService with notification, simple xml layout and RecordFragment.

---
+++ app/src/main/java/com/com/technoparkproject/RecordingService.java
@@ -0,0 +1,410 @@
+package com.com.technoparkproject;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.Binder;
+import android.os.Build;
+import android.os.IBinder;
+import android.text.format.DateUtils;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import com.com.technoparkproject.view.activities.MainActivity;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class RecordingService extends Service {
+
+    public interface OnRecordTimeListener{
+        void OnRecordTick(final long seconds);
+    }
+
+    private OnRecordTimeListener mRecordTimeListener;
+
+    public void setOnRecordTimeListener(OnRecordTimeListener onRecordTimeListener){
+        mRecordTimeListener = onRecordTimeListener;
+    }
+
+    private static final String CHANNEL_AUDIO_APP = "Voice-it channel";
+    private static final int FOREGROUND_ID = 1111;
+    public static int SAMPLING_RATE = 44100;
+    public static int AUDIO_SOURCE = MediaRecorder.AudioSource.MIC;
+    public static int CHANNEL_IN_CONFIG = AudioFormat.CHANNEL_IN_MONO;
+    public static int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
+    //public static int BYTE_BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLING_RATE, CHANNEL_IN_CONFIG, AUDIO_FORMAT); //minimum buffer size in bytes for AudioRecord
+    //public static final int SHORT_BUFFER_SIZE = BYTE_BUFFER_SIZE/2; //minimum buffer size in shorts for AudioRecord
+    private int mSampleSizeInBytes;
+    private int mFrameSizeInBytes;
+
+    ExecutorService mRecordingExecutor;
+    ScheduledExecutorService mScheduledExecutor;
+    private ScheduledFuture<?> mRecordFuture;
+    private Future<?> mRecordResult;
+    private AudioRecord mAudioRecord;
+    private FileOutputStream mRecordBOS;
+
+    private long mRecordTime; //record time in seconds
+
+
+    public static String START_FOREGROUND = "START_FOREGROUND";
+    public static String STOP_FOREGROUND = "STOP_FOREGROUND";
+
+
+
+    private volatile int mRecordStatus; //current status of the service, one of the constants
+
+    public static final int RECORD_STATUS_READY = 100;
+    public static final int RECORD_STATUS_RECORDING = 200;
+    public static final int RECORD_STATUS_PAUSED = 300;
+    public static final int RECORD_STATUS_STOPPED = 400;
+
+
+    private final IBinder mBinder = new RecordBinder();
+    private int mBufferSizeInBytes;
+
+    public int getStatus() {
+        return mRecordStatus;
+    }
+
+
+
+    public class RecordBinder extends Binder {
+        public RecordingService getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return RecordingService.this;
+        }
+    }
+
+    public boolean isReady(){
+        return mRecordStatus == RECORD_STATUS_READY;
+    }
+
+    public boolean isRecording(){
+        return mRecordStatus == RECORD_STATUS_RECORDING;
+    }
+
+    public boolean isPaused(){
+        return mRecordStatus == RECORD_STATUS_PAUSED;
+    }
+    public boolean isStopped(){
+        return mRecordStatus == RECORD_STATUS_STOPPED;
+    }
+
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        mScheduledExecutor = Executors.newSingleThreadScheduledExecutor();
+        mRecordingExecutor = Executors.newSingleThreadExecutor();
+        getBufferSize();
+        mRecordStatus = RECORD_STATUS_READY;
+    }
+
+    private void getBufferSize() {
+        mFrameSizeInBytes = AudioRecord.getMinBufferSize(SAMPLING_RATE, CHANNEL_IN_CONFIG, AUDIO_FORMAT); //minimum buffer size in bytes
+        int channelsCount = CHANNEL_IN_CONFIG == AudioFormat.CHANNEL_IN_MONO ? 1 : 2;
+        int bitDepthInBytes = AUDIO_FORMAT == AudioFormat.ENCODING_PCM_16BIT ? 2 : 1;
+        mSampleSizeInBytes = channelsCount * bitDepthInBytes;
+        mBufferSizeInBytes = mFrameSizeInBytes*5; //for smoother recording
+        /*int minFrameSize = bufferSizeInBytes / mSampleSizeInBytes;
+        int minFramesInSecond = SAMPLING_RATE / minFrameSize;
+        int framesInSecond = (minFramesInSecond / 10) * 10; //align min frame size to lower multiple of 10
+        mFrameSizeInBytes = SAMPLING_RATE / framesInSecond * mSampleSizeInBytes;*/
+    }
+
+
+
+    private void createNotificationChannel() {
+        //notification channel is needed for API >= 26
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            if (notificationManager.getNotificationChannel(CHANNEL_AUDIO_APP)!=null)
+                return;
+            CharSequence name = getString(R.string.channel_audio_app);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_AUDIO_APP, name, importance);
+            channel.setDescription(description);
+            channel.setSound(null,null);//disable sounds for this channel
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void updateNotification(String text){
+        Notification recordNotification = buildForegroundNotification(text);
+        NotificationManager notificationManager =
+                (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(FOREGROUND_ID,recordNotification);
+
+    }
+
+    private Notification buildForegroundNotification(String text) {
+        NotificationCompat.Builder builder=
+                new NotificationCompat.Builder(this, CHANNEL_AUDIO_APP);
+
+        //disable sound using builder if API < 26
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O){
+            builder.setSound(null);
+        }
+
+        builder.setOngoing(true)
+                .setContentTitle(getString(R.string.audio_record))
+                .setContentText(text)
+                .setSmallIcon(android.R.drawable.presence_audio_busy);
+
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.putExtra("showFragment", "RecordFragment");
+        PendingIntent pendingIntent=PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+
+        return(builder.build());
+    }
+
+
+    //start/stops the service in foreground mode
+    public void runForeground(boolean isForeground){
+        if (isForeground){
+            createNotificationChannel();
+            startForeground(FOREGROUND_ID,
+                    buildForegroundNotification("Имя записи..."));
+        }
+        else{
+            stopForeground(true);
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent == null)
+            return START_STICKY;
+
+
+
+        return START_STICKY;
+    }
+
+
+    public void startRecording() {
+        File recordFile = createTempFile();
+
+        //buffer size can store 5x  buffer size
+        //for smoother recording than with the minBufferSize value
+        //int bufferSizeInBytes = BYTE_BUFFER_SIZE * 4;
+
+        mRecordTime = 0;
+
+        mAudioRecord = new AudioRecord(AUDIO_SOURCE,
+                SAMPLING_RATE, CHANNEL_IN_CONFIG,
+                AUDIO_FORMAT, mBufferSizeInBytes);
+
+        //notification period is 1 second
+        //so clients may use it as a timer callback
+        mAudioRecord.setPositionNotificationPeriod(SAMPLING_RATE);
+
+        mAudioRecord.setRecordPositionUpdateListener(new AudioRecord.OnRecordPositionUpdateListener() {
+            @Override
+            public void onMarkerReached(AudioRecord recorder) {
+                //not using marker callback for now
+            }
+
+            @Override
+            public void onPeriodicNotification(AudioRecord recorder) {
+                if (mRecordTimeListener == null){
+                    Log.e("OnRecordTimeListener","OnRecordTimeListener is not set on RecordingService");
+                    return;
+                }
+                mRecordTime++;
+                updateNotification("Имя записи..."+ DateUtils.formatElapsedTime(mRecordTime));
+                mRecordTimeListener.OnRecordTick(mRecordTime);
+            }
+        });
+
+        if (mAudioRecord.getState() ==
+                AudioRecord.STATE_UNINITIALIZED)
+            Log.e("AudioRecord init error", "AudioRecord state is uninitialized after constructor called");
+
+        mRecordBOS = null;
+        try {
+            mRecordBOS = new FileOutputStream(recordFile);
+            //mRecordBOS = new BufferedOutputStream(recordFOS);
+        } catch (FileNotFoundException e) {
+            Log.e("FileOutputStream", "File not found for recording ", e);
+        }
+        resumeRecording();
+    }
+
+
+    public void resumeRecording(){
+        mRecordStatus = RECORD_STATUS_RECORDING;
+
+        FileChannel recordFileChannel = mRecordBOS.getChannel();
+
+        //Create Direct ByteBuffer with native endianness that should be Little Endian for most devices
+        ByteBuffer audioBuffer = ByteBuffer.allocateDirect(mBufferSizeInBytes).order(ByteOrder.nativeOrder());
+        //short[] testBuf = new short[mBufferSizeInBytes];
+
+        mAudioRecord.startRecording();
+
+        RecordTask recordTask = new RecordTask(mAudioRecord, recordFileChannel, audioBuffer);
+
+        //int samplesInFrame = mFrameSizeInBytes / mSampleSizeInBytes;
+        /*mRecordFuture = mScheduledExecutor.scheduleAtFixedRate(recordTask, 0,
+                SAMPLING_RATE/mFrameSizeInBytes
+                ,TimeUnit.MILLISECONDS);*/
+        mRecordResult = mRecordingExecutor.submit(recordTask);
+    }
+
+    public void pauseRecording(){
+        mRecordStatus = RECORD_STATUS_PAUSED;
+        if (mAudioRecord == null) {
+            Log.e("pauseRecording", "AudioRecord is null!");
+            return;
+        }
+        try {
+            //wait till record task is finished
+            if (mRecordResult.get() == null)
+                mAudioRecord.stop();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopRecording(){
+        pauseRecording();
+        mRecordStatus = RECORD_STATUS_STOPPED;
+        mAudioRecord.release();
+        mAudioRecord = null;
+        try {
+            mRecordBOS.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mRecordStatus = RECORD_STATUS_READY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mScheduledExecutor.shutdown();
+        mRecordingExecutor.shutdown();
+        if (mRecordTimeListener != null)
+            mRecordTimeListener = null;
+        /*
+        //TODO: Don't know if needed??
+        try {
+            mScheduledExecutor.awaitTermination(1,TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+         */
+    }
+
+
+    private static byte[] shortToByteLE(short x){
+        byte[] ret = new byte[2];
+        ret[0] = (byte)(x & 0xff);
+        ret[1] = (byte)((x >> 8) & 0xff);
+        return ret;
+    }
+
+    private static byte[] shortArrToByteArr(short[] shorts){
+        byte[] bytes = new byte[shorts.length*2];
+        for (int i = 0; i< shorts.length ; i++){
+            bytes[2*i] = (byte)(shorts[i] & 0xff);
+            bytes[2*i+1] = (byte)((shorts[i] >> 8) & 0xff);
+        }
+        return bytes;
+    }
+
+    public class RecordTask implements Runnable {
+
+        private final ByteBuffer mAudioBuffer;
+        private final AudioRecord mAudioRecord;
+        private final FileChannel mRecordFileChannel;
+
+        public RecordTask(final AudioRecord audioRecord, final FileChannel recBOS, final ByteBuffer audioBuffer) {
+            mAudioRecord = audioRecord;
+            mRecordFileChannel = recBOS;
+            mAudioBuffer = audioBuffer;
+        }
+
+
+
+        @Override
+        public void run() {
+           while (isRecording()) {
+                Log.d("Task", "new call");
+                int bytesRead = mAudioRecord.read(mAudioBuffer, mBufferSizeInBytes);
+                //int bytesRead = mAudioRecord.read(test,0,test.length );
+                Log.d("BytesRead", String.valueOf(bytesRead));
+                //bytesRead indicates number of bytes OR error status;
+                if (bytesRead == AudioRecord.ERROR_INVALID_OPERATION ||
+                        bytesRead == AudioRecord.ERROR_BAD_VALUE) {
+                    Log.e("AudioRecord", "Error reading audio data!");
+                    return;
+                }
+                try {
+                    mAudioBuffer.limit(bytesRead); //manually set limit as it's not clear if it's set correctly
+                    //TODO: test limit and other pointers after read
+
+                    //write in loop in case not full buffer is written immediately
+                    while (mAudioBuffer.hasRemaining())
+                        mRecordFileChannel.write(mAudioBuffer);
+                        //mRecordBOS.write(shortArrToByteArr(mAudioBuffer));
+                } catch (IOException e) {
+                    Log.e("RecordFileChannel", "Error writing to the channel ", e);
+                    return;
+                }
+                Log.d("Task", "new finish");
+                mAudioBuffer.clear();
+           }
+        }
+    }
+
+    File createTempFile() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH);
+        String fileName = sdf.format(new Date());
+        try {
+            File tempFileDir = this.getCacheDir();
+            File tempRecordFile = File.createTempFile(fileName, ".pcm", tempFileDir);
+            return tempRecordFile;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- app/src/main/java/com/com/technoparkproject/view/fragments/RecordFragment.java
+++ app/src/main/java/com/com/technoparkproject/view/fragments/RecordFragment.java
@@ -1,25 +1,196 @@
 package com.com.technoparkproject.view.fragments;
 
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+
+import android.os.IBinder;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.com.technoparkproject.R;
 import com.com.technoparkproject.view.activities.MainActivity;
+import com.com.technoparkproject.RecordingService;
 
-public class RecordFragment extends Fragment {
+import org.jetbrains.annotations.NotNull;
+
+
+public class RecordFragment extends Fragment implements ServiceConnection, RecordingService.OnRecordTimeListener {
+    private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 1000;
+    RecordingService mRecService;
+    boolean mBound = false;
+    private Button mRecPauseButton;
+    private Button mStopButton;
+    private TextView mTimeTextView;
+
+    // Requesting permission to RECORD_AUDIO
+    //private boolean permissionToRecordAccepted = false;
+    //private String[] permissions = {Manifest.permission.RECORD_AUDIO};
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void checkRecordPermissions() {
+        if (requireActivity().checkSelfPermission(Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO},
+                    PERMISSIONS_REQUEST_RECORD_AUDIO);
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NotNull String[] permissions,
+                                           @NotNull int[] grantResults) {
+        if (requestCode != PERMISSIONS_REQUEST_RECORD_AUDIO
+                || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+
+            //TODO: imitate exiting fragment or other behaviour
+            TextView textView = requireActivity().findViewById(R.id.sample_record_text);
+            textView.setText(R.string.text_record_permission_denied);
+        }
+    }
+
 
     private static final int FRAGMENT_RECORD_NAME = R.string.fragment_record_name;
 
-    @Nullable
+    public void onStart() {
+        super.onStart();
+        Intent recServiceIntent = new Intent(getActivity(), RecordingService.class);
+        // start service before binding
+        // to handle its lifecycle in an unbound manner
+        requireActivity().startService(recServiceIntent);
+        requireActivity().bindService(recServiceIntent, this, Context.BIND_DEBUG_UNBIND);
+    }
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public void onStop() {
+        super.onStop();
+        if (mBound) {
+            requireActivity().unbindService(this);
+            mBound = false;
+        }
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+        //get Recording Service instance
+        RecordingService.RecordBinder binder = (RecordingService.RecordBinder) service;
+        mRecService = binder.getService();
+        mBound = true;
+        mRecService.setOnRecordTimeListener(this);
+        activateButtons();
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+        mBound = false;
+        mRecService = null;
+    }
+
+
+    private void setButtonsEnabled(){
+        mRecPauseButton.setEnabled(mBound);
+        mRecPauseButton.setEnabled(mBound);
+    }
+
+    private void setButtonsState() {
+        if (mRecService != null){
+            switch (mRecService.getStatus()){
+                case RecordingService.RECORD_STATUS_PAUSED:{
+                    mRecPauseButton.setText("Запись");
+                    break;
+                }
+                case RecordingService.RECORD_STATUS_RECORDING:{
+                    mRecPauseButton.setText("Пауза");
+                    break;
+                }
+                default:{
+                    mRecPauseButton.setText("Запись");
+                }
+
+            }
+        }
+    }
+
+
+    private void activateButtons() {
+        if (mRecService == null)
+            throw new NullPointerException("mRecService is null, can't setup listeners");
+        setButtonsEnabled();
+        setButtonsState();
+
+        mRecPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mRecService.isReady()) {
+                    //mRecPauseButton.setText("Пауза");
+                    mRecService.startRecording();
+
+                    //make service run foreground during recording
+                    mRecService.runForeground(true);
+                }
+                else
+                    if (mRecService.isRecording()) {
+                       // Log.d("cvxc", "xcvxcvx");
+                        //mRecPauseButton.setText("Запись");
+                        mRecService.pauseRecording();
+                    }
+                    else
+                        if (mRecService.isPaused()) {
+                            //mRecPauseButton.setText("Пауза");
+                            mRecService.resumeRecording();
+                        }
+                setButtonsState();
+            }
+        });
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mRecService.isPaused() || mRecService.isRecording()) {
+                    //mRecPauseButton.setText("Запись");
+                    mRecService.stopRecording();
+                    //remove service from foreground, recording is stopped
+                    mRecService.runForeground(false);
+                }
+                setButtonsState();
+            }
+        });
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         ((MainActivity) getActivity()).setToolbar(getString(FRAGMENT_RECORD_NAME));
-        return inflater.inflate(R.layout.fragment_record, container, false);
+        View view = inflater.inflate(R.layout.fragment_record, container, false);
+        mRecPauseButton = view.findViewById(R.id.record_pause_button);
+        //mRecPauseButton.setText("Запись");
+        mStopButton = view.findViewById(R.id.stop_button);
+        setButtonsEnabled();
+        mStopButton.setText("Стоп");
+        mTimeTextView = view.findViewById(R.id.record_time_text);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkRecordPermissions();
+        }
+
+        return view;
+    }
+
+    @Override
+    public void OnRecordTick(long seconds) {
+        if (mTimeTextView!= null)
+            mTimeTextView.setText(DateUtils.formatElapsedTime(seconds));
     }
 }
