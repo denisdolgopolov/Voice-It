ebeb617f7a506606e4631bc3e06f66aef2c9adcd
siiena25 <yuliacoyulia@yandex.ru>
2020-12-17 22:50:37 +0300

    Correction

--- app/src/main/java/com/com/technoparkproject/view/activities/MainActivity.java
+++ app/src/main/java/com/com/technoparkproject/view/activities/MainActivity.java
@@ -36,7 +36,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.technopark.recorder.service.RecordIntentConstants;
 
 public class MainActivity extends AppCompatActivity {
