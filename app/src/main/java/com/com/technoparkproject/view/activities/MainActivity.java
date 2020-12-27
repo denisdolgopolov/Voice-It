340549443ec62f36fd2f409925aa91968ca03009
Julia Kostyunina <54817988+siiena25@users.noreply.github.com>
2020-12-27 18:57:44 +0300

    Update MainActivity.java

--- app/src/main/java/com/com/technoparkproject/view/activities/MainActivity.java
+++ app/src/main/java/com/com/technoparkproject/view/activities/MainActivity.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
