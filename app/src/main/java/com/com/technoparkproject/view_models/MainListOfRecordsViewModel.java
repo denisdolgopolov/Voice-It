eff1ef780cef17cdfb10ee31dcfe11e91ba8efd2
IDolgopolov <dendolg1@mail.ru>
2020-12-28 22:47:10 +0300

    --fix item clicked

--- app/src/main/java/com/com/technoparkproject/view_models/MainListOfRecordsViewModel.java
+++ app/src/main/java/com/com/technoparkproject/view_models/MainListOfRecordsViewModel.java
@@ -20,6 +20,7 @@ import com.com.technoparkproject.repo.AppRepoImpl;
 import com.com.technoparkproject.repo.LoadStatus;
 import com.example.player.PlayerServiceConnection;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainListOfRecordsViewModel extends AndroidViewModel {
@@ -70,25 +71,21 @@ public class MainListOfRecordsViewModel extends AndroidViewModel {
     }
 
     public void addToPlaylistClicked(Record record) {
-        //TODO
         playerServiceConnection.addToPlaylist(PlayerConverter.toPlayerRecord(record));
     }
 
 
     public void itemClicked(Record record) {
         playerServiceConnection.clearPlaylist();
-        int currentIndex = 0;
+        List<Record> tempPlaylist = new ArrayList<>();
         ArrayMap<Topic, List<Record>> list = topicRecords.getValue();
         for (int i = 0; i < list.size(); i++) {
-            for (int g = 0; g < list.valueAt(i).size(); g++) {
-                Record currentRecord = list.valueAt(i).get(g);
-                playerServiceConnection.addToPlaylist(PlayerConverter.toPlayerRecord(currentRecord));
-                if (currentRecord.equals(record)) {
-                    playerServiceConnection.setCurrentIndex(currentIndex);
-                }
-            }
-            currentIndex++;
+            tempPlaylist.addAll(list.valueAt(i));
         }
+        for (int i = 0; i < tempPlaylist.size(); i++) {
+            playerServiceConnection.addToPlaylist(PlayerConverter.toPlayerRecord(tempPlaylist.get(i)));
+        }
+        playerServiceConnection.setCurrentIndex(tempPlaylist.indexOf(record));
         playerServiceConnection.mediaController.getTransportControls().play();
     }
 
