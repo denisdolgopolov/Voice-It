0e0a206348ed1b4c89118c37bd63dfc11877c174
Julia Kostyunina <54817988+siiena25@users.noreply.github.com>
2020-12-29 01:18:50 +0300

    Update AnotherAccountFragment.java

--- app/src/main/java/com/com/technoparkproject/view/fragments/AnotherAccountFragment.java
+++ app/src/main/java/com/com/technoparkproject/view/fragments/AnotherAccountFragment.java
@@ -11,7 +11,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
